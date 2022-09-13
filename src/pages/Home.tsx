bd158d066efaba14bacf8fe8889720a719c30186
Anastasios Orfanidis <anastasios.orfanidis@gmail.com>
2022-09-14 00:18:30 +0100

    Home and Cta component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,9 @@
-import React from 'react';
-import Card from '../components/Card';
-import dummyData from "../dummyData.json"; // To be replaced with your api response data
-
+import { Cta } from '../components/Cta';
 
 export const Home = () => {
   return (
     <>
-      <h1>Space X Ships</h1>
-      <div className="App" style={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr", rowGap: "10px", columnGap: "20px"}}>
-        <Card image={dummyData.image} name={dummyData.name} home_port={dummyData.home_port} roles={dummyData.roles} />
-      </div>
+      <Cta />
     </>
   );
 };
