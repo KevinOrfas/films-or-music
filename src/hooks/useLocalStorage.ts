ccce7cd1c29f72e79e388895aa30a5f969f81405
Anastasios Orfanidis <anastasios.orfanidis@gmail.com>
2022-09-13 12:20:23 +0100

    Update local storage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -11,7 +11,11 @@ export function useLocalStorage<T>(key: string, initialValue: T) {
       // Get from local storage by key
       const item = window.localStorage.getItem(key);
       // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue;
+      return item
+        ? typeof item === 'string'
+          ? item
+          : JSON.parse(item)
+        : initialValue;
     } catch (error) {
       // If error also return initialValue
       console.log(error);
