61595de78c350620842a22398d677f0f7a1400d8
Anastasios Orfanidis <anastasios.orfanidis@gmail.com>
2022-09-13 01:07:32 +0100

    Webpack export as function to add env variables and set up config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,56 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require('dotenv');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: path.join(__dirname, "src", "index.js"),
-  output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
-  mode: process.env.NODE_ENV || "development",
-  resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
-  devServer: { static: path.join(__dirname, "src") },
-  plugins: [
-    new HtmlWebpackPlugin({
-        template: path.join(__dirname, "public", "index.html"),
-    }),
-  ],
-  module: {
-    rules: [
-        { 
-            test: /\.(js|jsx)$/, 
-            exclude: /node_modules/, 
-            use: ["babel-loader"] 
+module.exports = () => {
+  const env = dotenv.config().parsed;
+
+  // reduce it to a nice object, the same as before
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'index.bundle.js',
+    },
+    mode: process.env.NODE_ENV || 'development',
+    resolve: { modules: [path.resolve(__dirname, 'src'), 'node_modules'] },
+    devServer: { static: path.join(__dirname, 'src') },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'public', 'index.html'),
+      }),
+      new webpack.DefinePlugin(envKeys),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
         },
         {
           test: /\.(ts|tsx)$/,
           exclude: /node_modules/,
-          use: ["ts-loader"],
+          use: ['ts-loader'],
         },
         {
-            test: /\.(css|scss)$/,
-            use: ["style-loader", "css-loader"],
+          test: /\.(css|scss)$/,
+          use: ['style-loader', 'css-loader'],
         },
-        { 
-            test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-            use: ["file-loader"],
+        {
+          test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+          use: ['file-loader'],
         },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  }
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+  };
 };
